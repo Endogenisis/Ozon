23f26cddc5157b3436fc66fea9c01288148d5b09
Endogenisis <96134799+Endogenisis@users.noreply.github.com>
2022-03-28 13:48:50 +0300

    Поменял формулу подсчёта.

--- Палиндром.py
+++ Палиндром.py
@@ -1,17 +1,11 @@
 n=int(input())
-m=0
+m=n
 l = list(map(int, str(n)))
 d=l[::-1]
-while len(l) <= 1:
-    n+=1
-    m+=1
-    l = list(map(int, str(n)))
-    d=l[::-1]
 while n <= 10**13:
     if l != d:
         n+=1
-        m+=1
         l = list(map(int, str(n)))
         d=l[::-1]
     else: break
-print(m)
+print(n-m)
