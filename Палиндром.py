456425ef068fd591afc56523a113f5df0cebc8fd
Endogenisis <96134799+Endogenisis@users.noreply.github.com>
2022-03-28 14:16:15 +0300

    Убрал списки, сделал строки.

--- Палиндром.py
+++ Палиндром.py
@@ -1,11 +1,11 @@
 n=int(input())
 m=n
-l = list(map(int, str(n)))
+l = str(n)
 d=l[::-1]
 while n <= 10**13:
     if l != d:
         n+=1
-        l = list(map(int, str(n)))
+        l = str(n)
         d=l[::-1]
     else: break
 print(n-m)
