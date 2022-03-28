ef0e6460c548206a7944770b04421482b3a785c0
Endogenisis <96134799+Endogenisis@users.noreply.github.com>
2022-03-28 13:04:20 +0300

    Финальная версия.

--- Пин-код.py
+++ Пин-код.py
@@ -1,7 +1,11 @@
 n=str(input())
-print(int(n))
-print(int(n[0]+n[2]+n[1]))
-print(int(n[1]+n[0]+n[2]))
-print(int(n[1]+n[2]+n[0]))
-print(int(n[2]+n[0]+n[1]))
-print(int(n[2]+n[1]+n[0]))
+l=[]
+l.append(int(n))
+l.append(int(n[0]+n[2]+n[1]))
+l.append(int(n[1]+n[0]+n[2]))
+l.append(int(n[1]+n[2]+n[0]))
+l.append(int(n[2]+n[0]+n[1]))
+l.append(int(n[2]+n[1]+n[0]))
+l.sort()
+for x in l:
+    print(x)
